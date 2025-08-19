6e119e65bc9b4b545d4ef61467bad7b3c7b359ea
YANNAM BHAVANI <bhavaniyannam443@gmail.com>
2025-08-19 10:50:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ st.set_page_config(page_title="Tic-Tac-Toe", layout="centered")
 st.title("🎮 Tic-Tac-Toe Game")
 st.write("Enjoy playing the classic game!")
 
-with open("index.html", "r", encoding="utf-8") as f:
+with open("tictactoe.html", "r", encoding="utf-8") as f:
     html_data = f.read()
 
 st.components.v1.html(html_data, height=600)
